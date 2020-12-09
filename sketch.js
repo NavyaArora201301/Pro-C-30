740a1f293132b092a65892994c4777f20efb9f83
NavyaArora201301 <71163478+NavyaArora201301@users.noreply.github.com>
2020-12-09 15:03:34 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,153 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var block1, block2;
+var block3, block4;
+var block5, block6;
+var block7, block8;
+var block9; block10;
+var block11,block12;
+var block13, block14;
+var block15, block16;
+var block17, block18;
+var block19, block20;
+var block21, block22;
+
+var player;
+var ground1, ground2, ground3;
+
+var sling;
+var player;
+
+var ball;
+var img;
+
+function preload(){
+  img = loadImage("polygon.png");
+}
+
+function setup() {
+  createCanvas(1500,600);
+  engine = Engine.create();
+    world = engine.world;
+
+
+  block1 = new Block(600,260,30,40);
+  block2 = new Block(570,260,30,40);
+  block3 = new Block(540,260,30,40);
+  block4 = new Block(630,260,30,40);
+  block5 = new Block(660,260,30,40);
+
+
+
+  block6 = new Block(585,220,30,40);
+  block7 = new Block(555,220,30,40);
+  block8 = new Block(615,220,30,40);
+  block9 = new Block(645,220,30,40);
+
+
+
+  bolck10 = new Block(600,170,30,40);
+  block11 = new Block(570,180,30,40);
+  block12 = new Block(630,180,30,40);
+
+
+  block13 = new Block(600,140,30,40);
+
+  ground1 = new Ground(600,285,200,10);
+  ground2 = new Ground(900,195,200,10);
+  ground3 = new Ground(750, 600, 1500, 10)
+
+
+  block14 = new Block(900,170,30,40);
+  block15 = new Block(930,170,30,40);
+  block16 = new Block(870,170,30,40);
+  block17 = new Block(840,170,30,40);
+  block18 = new Block(960,170,30,40);
+
+  block19 = new Block(900,140,30,40);
+  block20 = new Block(930,140,30,40);
+  block21 = new Block(870,140,30,40);
+
+  block22 = new Block(900,110,30,40);
+
+  player = new Player(50,200,30,30);
+
+  ball = Bodies.circle(50,200,20);
+  World.add(world,ball);
+
+  sling = new Chain(this.ball,{x:150, y:160});
+
+}
+
+function draw() {
+  background("indigo");
+  Engine.update(engine);
+
+  fill("cyan");
+  block1.display();
+  block2.display();
+  block3.display();
+  block4.display();
+  block5.display();
+
+  fill("lime");
+  block6.display();
+  block7.display();
+  block8.display();
+  block9.display();
+
+  fill("magenta");
+  bolck10.display();
+  block11.display();
+  block12.display();
+
+  fill("white");
+  block13.display();
+
+  fill("red");
+  block14.display();
+  block15.display();
+  block16.display();
+  block17.display();
+  block18.display();
+
+  fill("blue");
+  block19.display();
+  block20.display();
+  block21.display();
+
+  fill("yellow");
+  block22.display();
+
+
+  ground1.display();
+  ground2.display();
+  ground3.display();
+
+  imageMode(CENTER);
+  image(img,ball.position.x,ball.position.y,40,40);
+
+  sling.display();
+}
+
+
+function mouseDragged(){
+  Matter.Body.setPosition(this.ball, {x: mouseX , y: mouseY});
+}
+
+
+function mouseReleased(){
+  sling.fly();
+}
+
+function keyPressed(){
+	if(keyCode === 32)
+	{
+		Matter.Body.setPosition(player.Body,{x: 235, y: 220})
+		sling.attach(player.body);
+	}
+}
